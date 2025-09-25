c1c5de0562de947488d16133470e7d029b164db8
AfreedHassan <afreed.hassan16@gmail.com>
2025-09-24 23:52:34 -0400

    Initial Commit

---
+++ seventy-twoTests/seventy_twoTests.swift
@@ -0,0 +1,17 @@
+//
+//  seventy_twoTests.swift
+//  seventy-twoTests
+//
+//  Created by Mir Afreed Hassan on 2025-09-24.
+//
+
+import Testing
+@testable import seventy_two
+
+struct seventy_twoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
